4ef28fcac695e8e43000fcb756724d122e754dcc
Thapelo Monareng <Thapelo.Monareng@azuro.co.za>
2019-09-05 14:12:30 +0200

    refactor a bit

--- tradegecko.fileprocessor/wwwroot/js/site.js
+++ tradegecko.fileprocessor/wwwroot/js/site.js
@@ -1,54 +1,62 @@
 ﻿$(function () {
     $('input#search').click(function (e) {
         e.preventDefault();
-
-        $.ajax({
-            url: '/api/ObjectTransaction',
-            type: 'get',
-            data: $('form').serialize(),
-            contentType: false,
-            processData: false,
-            success: function (response) {
-                $('div#searchResults').empty();
-                $('#error_section').hide();
-                $.each(response, function (index) {
-                    $('div#searchResults').append('<h5>********Object Results**************</h5>');
-                    $('div#searchResults').append('<p> Object Type ---> ' + this.objectType + '</p>');
-                    $('div#searchResults').append('<p> Object Changes ---> ' + this.objectChanges + '</p>');
-                });
-            },
-            error: function (error) {
-                $('#error_section').show();
-                $('#error_code').text(error.status);
-                $('#error_msg').text(error.statusText);
-                $('#res_msg').text(error.responseText);
-            }
-        });
+        searchObjectState();
     });
 
     $('input#uploadFile').click(function (e) {
         e.preventDefault();
-        var formData = new FormData();
-        var file = $('#file')[0].files[0]; 
-        formData.append('file', file); 
-
-        $.ajax({
-            url: '/api/ObjectTransaction',
-            type: 'post',
-            data: formData,
-            contentType: false,
-            processData: false,
-            success: function (response) {
-                $('#file_upload_section').hide();
-                $('#error_section').hide();
-                $('#upload_success_section').show();
-            },
-            error: function (error) {
-                $('#error_section').show();
-                $('#error_code').text(error.status);
-                $('#error_msg').text(error.statusText);
-                $('#res_msg').text(error.responseText);
-            }
-        }); 
+        uploadFiles(); 
     });
 });
+
+function displayProcessingError(error) {
+    $('#error_section').show();
+    $('#error_code').text(error.status);
+    $('#error_msg').text(error.statusText);
+    $('#res_msg').text(error.responseText);
+}
+
+function uploadFiles() {
+    var formData = new FormData();
+    var file = $('#file')[0].files[0];
+    formData.append('file', file);
+
+    $.ajax({
+        url: '/api/ObjectTransaction',
+        type: 'post',
+        data: formData,
+        contentType: false,
+        processData: false,
+        success: function (response) {
+            $('#file_upload_section').hide();
+            $('#error_section').hide();
+            $('#upload_success_section').show();
+        },
+        error: function (error) {
+            displayProcessingError(error);
+        }
+    });
+}
+
+function searchObjectState() {
+    $.ajax({
+        url: '/api/ObjectTransaction',
+        type: 'get',
+        data: $('form').serialize(),
+        contentType: false,
+        processData: false,
+        success: function (response) {
+            $('div#searchResults').empty();
+            $('#error_section').hide();
+            $.each(response, function (index) {
+                $('div#searchResults').append('<h5>********Object Results**************</h5>');
+                $('div#searchResults').append('<p> Object Type ---> ' + this.objectType + '</p>');
+                $('div#searchResults').append('<p> Object Changes ---> ' + this.objectChanges + '</p>');
+            });
+        },
+        error: function (error) {
+            displayProcessingError(error);
+        }
+    });
+}
